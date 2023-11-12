00f16e3c226ff33e620101ac7653c9341a7d1836
warhammer21 <shreyakvashisht21@gmail.com>
2023-11-12 00:25:07 -0800

    model registry

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
-FROM continuumio/anaconda3:4.4.0
+FROM  continuumio/anaconda3:4.4.0
 MAINTAINER UNP, https://unp.education
-COPY ./rf_flask_project/usr/local/python/
+#WORKDIR /opt/rf_flask_project
+
+COPY ./rf_flask_project /usr/local/python/
+#ADD ./rf_flask_project /opt/rf_flask_project/
+
 EXPOSE 5000
 WORKDIR /usr/local/python/
 RUN pip install -r requirements.txt
-CMD python flask_predict_api.py
+CMD python flask_api.py
--- rf_flask_project/flask_api.py
+++ rf_flask_project/flask_api.py
@@ -4,10 +4,33 @@ from flasgger import Swagger
 import numpy as np
 import pickle
 import pandas as pd
+# Random Forest Classifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import pickle
 
-path = '/rf.pkl'
-with open(path,'rb') as model_file:
-    model = pickle.load(model_file)
+
+# loading the dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Split the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.5)
+
+# Build the model
+model = RandomForestClassifier(n_estimators=10)
+# Train the classifier
+model.fit(X_train, y_train)
+
+
+
+# path = 'rf.pkl'
+# with open('rf.pkl','rb') as model_file:
+#     model = pickle.load(model_file)
 app = Flask(__name__)
 swagger = Swagger(app)
 @app.route("/predict")
